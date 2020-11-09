8a9bc40dbc6779dbc16e0bbc58f81db4a6e79f11
jclar <jclar@Snowpiercer>
2020-11-08 22:39:01 -0800

    ID update: Jess Clark

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 		System.out.println("Welcome to Git!");
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
-		System.out.println("SacStateID - This line was added by Jessica Clark.");
+		System.out.println("217308677 - This line was added by Jessica Clark.");
 		System.out.println("219223005 - This line was added by Dustin Nguyen.");
 		System.out.println("302177655 - This line was added by Skyler Bradley");
 		System.out.println("219340642 - This line was added by Brittney Scheven.");
