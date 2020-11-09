5254975898ad5e66e649c5aeaafabc518ae4b66a
ABEL <ABEL@10.0.0.221>
2020-11-09 00:20:47 -0800

    added id

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -9,7 +9,7 @@ public class Hello {
 		System.out.println("302177655 - This line was added by Skyler Bradley");
 		System.out.println("219340642 - This line was added by Brittney Scheven.");
 		System.out.println("220270935 - This line was added by Paneri Patel.");
-		System.out.println("SacStateID - This line was added by Abel Ontiveros.");
+		System.out.println("219918661 - This line was added by Abel Ontiveros.");
 		System.out.println("216633301 - This line was added by Brenton Kludt.");
 
 	}
