a0f5c1b5e9853844eb3a266b16101eb79db5cbf6
jclar <jclar@Snowpiercer>
2020-11-09 12:54:44 -0800

    removed student IDs

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -4,13 +4,13 @@ public class Hello {
 		System.out.println("Welcome to Git!");
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
-		System.out.println("217308677 - This line was added by Jessica Clark.");
-		System.out.println("219223005 - This line was added by Dustin Nguyen.");
-		System.out.println("302177655 - This line was added by Skyler Bradley");
-		System.out.println("219340642 - This line was added by Brittney Scheven.");
-		System.out.println("220270935 - This line was added by Paneri Patel.");
-		System.out.println("219918661 - This line was added by Abel Ontiveros.");
-		System.out.println("216633301 - This line was added by Brenton Kludt.");
+		System.out.println("This line was added by Jessica Clark.");
+		System.out.println("This line was added by Dustin Nguyen.");
+		System.out.println("This line was added by Skyler Bradley");
+		System.out.println("This line was added by Brittney Scheven.");
+		System.out.println("This line was added by Paneri Patel.");
+		System.out.println("This line was added by Abel Ontiveros.");
+		System.out.println("This line was added by Brenton Kludt.");
 
 	}
 
